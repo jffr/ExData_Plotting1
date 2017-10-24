33dfa350be20872efaa7a079e16f3b36f84e1ab9
Juan Flor <jfframos@gmail.com>
2017-10-24 15:33:26 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -14,9 +14,11 @@ twodays<-subset(consumption, as.Date(Date, "%d/%m/%Y")>=as.Date("2007-02-01", "%
 
 # Plot 3
 png(filename = "plot3.png", width = 480, height = 480, units = "px")
-with(twodays, plot(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering"))
-with(twodays, lines(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_1))
-with(twodays, lines(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_2, col = "red"))
-with(twodays, lines(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_3, col = "blue"))
-legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty = 1)
+with(twodays, {
+    plot(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+    lines(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_1)
+    lines(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_2, col = "red")
+    lines(strptime(paste(Date, Time), "%d/%m/%Y %H:%M:%S"), Sub_metering_3, col = "blue")
+    legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), lty = 1)
+})
 dev.off()
